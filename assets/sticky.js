01b3c8fae08990e571ca420afd98afbf29ad0d63
Eza Ap <eza.a.putra@gmail.com>
2021-02-22 21:23:54 +0700

    sticky navbar update

--- assets/sticky.js
+++ assets/sticky.js
@@ -1,12 +1,15 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
+var content = document.getElementById("content")
 var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    content.classList.add("sticky-content")
   } else {
     navbar.classList.remove("sticky");
+    content.classList.remove("sticky-content")
   }
 }
